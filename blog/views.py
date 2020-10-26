55e63741213041e34c1171bccac61fca508d7d38
faysal077 <shadmanfaysal053@gmail.com>
2020-10-26 19:20:25 +0600

    Chapter 2, Django 3 By Example

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
-from .models import Post
+from django.core.mail import send_mail,send_mass_mail
+from .forms import EmailPostForm, CommentForm
+from .models import Post, Comment
+from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 
-def post_list(request):
+def post_list(request, tag_slug = None):
     object_list = Post.objects.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag,slug = tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list,3)
     page = request.GET.get('page')
     try:
@@ -15,21 +25,97 @@ def post_list(request):
         posts=paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, '../templates/blog/post/list.html',
-                  {'page':page,
-                      'posts':posts})
+    return render(request, 'blog/post/list.html',{'page':page,'posts':posts,'tag':tag})
 
+"""
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published',
-                                    publish__year=year,
-                                    publish__month=month,
-                                    publish__day=day)
+    post = get_object_or_404(Post, slug=post,
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    # List of active comments of the post.
+    comments = post.comments.filter(active=True)
+    all_comment = Comment.objects.all()
+    new_comment = None
+    if request.method == 'POST':
+        #A Comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid:
+            # Create comment object but don't save to database yet.
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the DB
+            new_comment.save()
+        else:
+            comment_form = CommentForm()
     return render(request,
-                  '../templates/blog/post/detail.html',
-                  {'post': post})
+                  'blog/post/detail.html',{
+            'post':post,
+            'comments':comments,
+            'new_comment':new_comment,
+            'comment_form':comment_form,
+                  })
+
+"""
+def post_detail(request, year, month, day, post):
+  post = get_object_or_404(Post, slug=post,
+                                status='published',
+                                publish__year=year,
+                                publish__month=month,
+                                publish__day=day)
+  comments = post.comments.filter(active=True)
+  new_comment = None
+  if request.method == 'POST':
+    comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid:
+      new_comment = comment_form.save(commit=False)
+      new_comment.post = post
+      new_comment.save()
+  else:
+    comment_form = CommentForm()
+
+  post_tags_ids = post.tags.values_list('id', flat=True)
+  similar_posts = Post.objects.filter(tags__in=post_tags_ids).exclude(id=post.id)
+  similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+  return render(request,
+                'blog/post/detail.html',
+                {'post': post,
+                 'comments': comments,
+                 'new_comment': new_comment,
+                 'comment_form': comment_form,
+                 'similar_posts': similar_posts})
+
 
 class PostListView(ListView):
     queryset = Post.objects.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    to = ""
+    name = ""
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
--- covid/settings.py
+++ covid/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'bravolulu07@gmail.com'
+EMAIL_HOST_PASSWORD = '@12345@Bangladesh#'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
